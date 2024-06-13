fc228dc74f9cb74867997e2e8f4b613aa759da84
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-06-13 16:55:34 -0300

    first commit

--- database/bd.js
+++ database/bd.js
@@ -12,11 +12,11 @@ const mysql = require('mysql2')
 // });
 
 const db = mysql.createPool({
-    host: 'monorail.proxy.rlwy.net',
-    port: '46885',
-    user: 'root',
-    password: 'jFOBxBwLuPcMCrHgLdOEHjIqSOmIYCMX',
-    database: 'railway',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 
