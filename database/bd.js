05241e561c051c5721376d90430a5d0363bdfb2c
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-06-20 15:45:09 -0300

    fazendo alguns ajustes

--- database/bd.js
+++ database/bd.js
@@ -1,15 +1,14 @@
 const dotenv = require('dotenv').config();
 console.log(dotenv.error);
 
-const mysql = require('mysql2');
+const { Pool } = require('pg');
 
-const db = mysql.createPool({
+const db = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
-
 
 module.exports = db;
